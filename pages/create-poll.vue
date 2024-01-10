d8f55fda0435327c76130b480759e77c3adc0a0e
hodame <hodame04@gmail.com>
2024-01-10 18:08:11 +0500

    imporive validation

--- pages/create-poll.vue
+++ pages/create-poll.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
-import { array, object, string } from 'yup'
+import { array, date, lazy, object, string } from 'yup'
 
 import type { CreatePollForm } from '~/components/create-poll/form.vue'
-import type { CreatePollRequest } from '~/repository/modules/PollModule/types';
+import type { CreatePollRequest } from '~/repository/modules/PollModule/types'
+
+definePageMeta({
+  middleware: 'auth'
+})
 
 const validationSchema = object().shape({
+  dates: lazy((val) =>
+    !Array.isArray(val)
+      ? date().min(new Date(), "Date can't be in the past")
+      : array()
+          .of(date().min(new Date(), "Dates can't be in the past"))
+          .required(),
+  ),
   description: string().required('Description is required'),
   options: array().of(string().required('Option value is required')),
   title: string().required('Title is required'),
@@ -17,7 +28,9 @@ const { handleSubmit } = useForm<CreatePollForm>({
 })
 
 const onSubmit = handleSubmit(async (values) => {
-  const dates = values.dates || []
+  const dates = Array.isArray(values.dates)
+    ? values.dates
+    : [undefined, values.dates]
 
   const startDate = dates[0] && dates[0].toISOString()
   const endDate = dates[1] && dates[1].toISOString()
@@ -40,10 +53,15 @@ const onSubmit = handleSubmit(async (values) => {
       <h1 class="text-2xl font-medium">Create a new poll</h1>
     </template>
 
-    <CreatePollForm @submit="onSubmit"/>
+    <CreatePollForm @submit="onSubmit" />
 
     <template #footer>
-      <ElButton class="w-full" native-type="submit" type="primary" @click="onSubmit">
+      <ElButton
+        class="w-full"
+        native-type="submit"
+        type="primary"
+        @click="onSubmit"
+      >
         Create poll
       </ElButton>
     </template>
